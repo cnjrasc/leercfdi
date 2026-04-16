b6c9f9b885693e338b2ebcf58d223743265deecc
JOSE JUAN LOPEZ MENDOZA <josejuanlopezmendoza@MacBook-Air-de-JOSE.local>
2026-04-16 14:50:57 -0600

    fix static and perfomnce

--- LEERCFDI/settings.py
+++ LEERCFDI/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+#Static files (CSS, JavaScript, Images)
+#https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
